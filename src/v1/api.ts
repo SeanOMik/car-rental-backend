e764cf36b67162298e45a1359b4f1a889450da46
SeanOMik <seanomik@gmail.com>
2024-04-11 17:36:16 -0400

    register the location endpoints in the api endpoint group

--- src/v1/api.ts
+++ src/v1/api.ts
@@ -2,10 +2,11 @@ import { Router } from "express";
 
 const user = require("./user");
 const vehicle = require("./vehicle");
+const location = require("./location");
 
 let router = Router();
 router.use("/user", user);
 router.use("/vehicle", vehicle);
-router.use("/location", vehicle);
+router.use("/location", location);
 
 module.exports = router;
